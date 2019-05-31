67996a98a8c212d170a68f4432c895e6cd053cd7
acl <acl447@nyu.edu>
2019-05-30 21:34:50 -0700

    commented out HTML route table id

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,23 +3,20 @@ let hookahFlavors = require("../models/hookah_flavors.js");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Table.findAll({}).then(function(dbExamples) {
+   
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
       });
     });
-  });
 
   // Load example page and pass in an example by id
-  app.get("/table/:id", function(req, res) {
+  // app.get("/table/:id", function(req, res) {
   
-    db.then(function(dbTable) {
-      res.render("table", {
-        table: dbTable
-      });
-    });
-  });
+  //   db.then(function(dbTable) {
+  //     res.render("table", {
+  //       table: dbTable
+  //     });
+  //   });
+  // });
 
   app.get("/reserve", function(req, res) {
     
