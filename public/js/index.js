530c632290d9bfb06af013ec6b201148e33b73f9
Matt Pengelly <matt.pengelly@edgenuity.com>
2019-06-01 13:38:48 -0700

    starting routes

--- public/js/index.js
+++ public/js/index.js
@@ -86,3 +86,32 @@ var handleFormSubmit = function(event) {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+let refreshFlavors = function() {
+  API.getFlavors().then(function(data) {
+    let flavorsList = data.map(function(table) {
+      var $a = $("<a>")
+        .text(example.text)
+        .attr("href", "/example/" + example.id);
+
+      var $li = $("<li>")
+        .attr({
+          class: "list-group-item",
+          "data-id": example.id
+        })
+        .append($a);
+
+      var $button = $("<button>")
+        .addClass("btn btn-danger float-right delete")
+        .text("ｘ");
+
+      $li.append($button);
+
+      return $li;
+    });
+
+    $exampleList.empty();
+    $exampleList.append($examples);
+  });
+};
+
