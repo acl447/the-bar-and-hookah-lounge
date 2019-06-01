20337b95fc661311ac479f8c220beb3bfa2cc155
Matt Pengelly <matt.pengelly@edgenuity.com>
2019-06-01 14:28:46 -0700

    trying to fix git nonsense

--- models/hookah_flavors.js
+++
@@ -1,9 +0,0 @@
-/*module.exports = function (sequelize, DataTypes) {
-  var Flavor = sequelize.define("flavor", {
-    name: DataTypes.STRING,
-    category: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    quantity: DataTypes.INTEGER
-  });
-  return Flavor;
-};*/
