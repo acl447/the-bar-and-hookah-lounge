5071ff2ef6be84bd97a41acc4061dac069b24b40
acl <acl447@nyu.edu>
2019-06-04 18:34:04 -0700

    vertically centered card on table.handlebars, commented out index.js code, added waitlist and reservations data

--- public/js/index.js
+++ public/js/index.js
@@ -1,106 +1,111 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+// $(function() {
+
+
 // Get references to page elements
-let $reserveTable = $("#reserveTable");
+// let $reserveTable = $("#reserveTable");
 
 // The API object contains methods for each kind of request we'll make
-let API = {
+// let API = {
   
-  getTables: function() {
-    return $.ajax({
-      url: "api/reservations",
-      type: "GET"
-    });
-  },
+//   getTables: function() {
+//     return $.ajax({
+//       url: "api/reservations",
+//       type: "GET"
+//     });
+//   },
 
-  reserveTable: function() {
+//   reserveTable: function() {
 
+//     return $.ajax({
 
-    return $.ajax({
+//       url: "api/tables/" 
+//     })
 
-      url: "api/tables/" 
-    })
+//   },
+//   updateTable: function() {
 
-  }
-  updateTable: function() {
+    // return $.ajax({
+    //   url: "api/tables/" + id,
+    //   type: "PUT",
+    //   data: updatedTable
+    // }).then(
+    //   function() {
 
-    return $.ajax({
-      url: "api/tables/" + id,
-      type: "PUT",
-      data: updatedTable
-    }).then(
-      function() {
-
-        console.log("updated table");
+    //     console.log("updated table");
 
         //Reload the manager page to get updated reservations list
-        location.assign("/manager");
-      }
-    );
-  },
-  getFlavors: function() {
+  //       location.assign("/manager");
+  //     }
+  //   );
+  // },
+  // getFlavors: function() {
 
-    return $.ajax({
-      url: "api/flavors",
-      type: "GET"
-    });
-  }
-};
+  //   return $.ajax({
+  //     url: "api/flavors",
+  //     type: "GET"
+  //   });
+//   }
+// };
 
 // refreshExamples gets new examples from the db and repopulates the list
-let refreshTables = function() {
-  API.getTables().then(function(data) {
-    let $tables = data.map(function(table) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+// let refreshTables = function() {
+//   API.getTables().then(function(data) {
+//     let $tables = data.map(function(table) {
+//       var $a = $("<a>")
+//         .text(example.text)
+//         .attr("href", "/example/" + example.id);
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
+//       var $li = $("<li>")
+//         .attr({
+//           class: "list-group-item",
+//           "data-id": example.id
+//         })
+//         .append($a);
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
+//       var $button = $("<button>")
+//         .addClass("btn btn-danger float-right delete")
+//         .text("ｘ");
 
-      $li.append($button);
+//       $li.append($button);
 
-      return $li;
-    });
+//       return $li;
+//     });
 
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
+//     $exampleList.empty();
+//     $exampleList.append($examples);
+//   });
+// };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
+// var handleFormSubmit = function(event) {
+//   event.preventDefault();
 
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
+//   var example = {
+//     text: $exampleText.val().trim(),
+//     description: $exampleDescription.val().trim()
+//   };
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
+//   if (!(example.text && example.description)) {
+//     alert("You must enter an example text and description!");
+//     return;
+//   }
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
+//   API.saveExample(example).then(function() {
+//     refreshExamples();
+//   });
 
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
+//   $exampleText.val("");
+//   $exampleDescription.val("");
+// };
 
 
-  });
-};
+//   });
+// };
+
+// }
 
 // Add event listeners to the submit and delete buttons
-$reserveTable.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+// $reserveTable.on("click", handleFormSubmit);
+// $exampleList.on("click", ".delete", handleDeleteBtnClick);
