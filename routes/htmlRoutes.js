d6efe7ed04d5d34d6431d76bccb7bbe6eae8cfb8
acl <acl447@nyu.edu>
2019-06-06 13:24:29 -0700

    update flavors

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,32 +1,32 @@
 let hookahFlavors = require("../models/hookah_flavors.js");
 let reservations = require("../models/tables.js");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-   
-      res.render("index", {
-      });
+  app.get("/", function (req, res) {
+
+    res.render("index", {
     });
+  });
 
   // Load table page and pass in a table by id
-  app.get("/table/:id", function(req, res) {
+  app.get("/table/:id", function (req, res) {
 
-      res.render("table", {
-        id: req.params.id
-      });
+    res.render("table", {
+      id: req.params.id
     });
+  });
 
-  app.get("/reserve", function(req, res) {
-    
-      res.render("reserve", {
-        
-      });
+  app.get("/reserve", function (req, res) {
+
+    res.render("reserve", {
+
     });
+  });
 
-  app.get("/manager", function(req, res) {
+  app.get("/manager", function (req, res) {
 
-    reservations.all(function(data) {
+    reservations.all(function (data) {
 
       let hbsObject = {
 
@@ -35,20 +35,33 @@ module.exports = function(app) {
       console.log(hbsObject);
       res.render("manager", hbsObject);
     });
-    
-     
-    });
 
-    app.get("/single-reservation", function(req, res) {
-    
-      res.render("single-reservation", {
-        
-      });
-    });
 
-  
+  });
+
+  app.get("/update-flavors", function (req, res) {
+
+    hookahFlavors.all(function (data) {
+
+      let hbsObject = {
+
+        hookahFlavorsList: data
+      };
+      console.log(hbsObject);
+      res.render("update-flavors", hbsObject);
+    });
+  });
+
+  app.get("/single-reservation", function (req, res) {
+
+    res.render("single-reservation", {
+
+    });
+  });
+
+
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-      res.render("404");
+  app.get("*", function (req, res) {
+    res.render("404");
   });
 };
