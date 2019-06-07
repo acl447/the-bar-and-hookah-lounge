216731c8543ae10f03738ceab541747ea9e27d8b
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-06-06 19:22:08 -0700

    cocktail js and orm/model changes for update

--- public/js/index.js
+++ public/js/index.js
@@ -4,14 +4,6 @@
 // Get references to page elements
 // let $reserveTable = $("#reserveTable");
 
-//Global variables
-let cocktails;
-let cocktailName;
-let description;
-let cocktailImg;
-let searchName;
-let counter = 1;
-
 // The API object contains methods for each kind of request we'll make
 
 let API = {
@@ -49,67 +41,6 @@ let API = {
       url: "api/flavors",
       type: "GET"
     });
-  },
-  randomCocktail: function () {
-    $.ajax({
-      url: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
-      method: "GET",
-    }).then(function (response) {
-      //console.log(response);
-
-      cocktails = response.drinks;
-      //console.log(cocktails);
-
-      for (let i = 0; i < cocktails.length; i++) {
-        cocktailName = cocktails[i].strDrink;
-
-        let ingredients = [cocktails[i].strIngredient1, cocktails[i].strIngredient2,
-        cocktails[i].strIngredient3, cocktails[i].strIngredient4, cocktails[i].strIngredient5,
-        cocktails[i].strIngredient6, cocktails[i].strIngredient7, cocktails[i].strIngredient8,
-        cocktails[i].strIngredient9, cocktails[i].strIngredient10];
-
-        description = ingredients.filter(function (el) {
-          return Boolean(el);
-        }).join(", ");
-
-        cocktailImg = cocktails[i].strDrinkThumb;
-
-        console.log(cocktailName);
-        console.log(description);
-        console.log(cocktailImg);
-
-        API.renderCocktails(cocktailImg, cocktailName, description);
-
-        if (counter < 3) {
-          counter++;
-          window.setTimeout(API.randomCocktail, 1000);
-        }
-      };
-    });
-  },
-  renderCocktails: function (cocktailImg, cocktailName, description) {
-    $("#cocktails").prepend("<hr>");
-
-    let descDrink = $("<p>");
-    descDrink.addClass("description card-text");
-    descDrink.attr("data-drink-description", description);
-    descDrink.attr("id", "descColor");
-    descDrink.text(description);
-    $("#cocktails").prepend(descDrink);
-
-    let drinkName = $("<h5>");
-    drinkName.addClass("drinkName card-title text-center");
-    drinkName.attr("data-drink-name", cocktailName);
-    drinkName.attr("id", "cNameColor")
-    drinkName.text(cocktailName);
-    $("#cocktails").prepend(drinkName);
-
-    let imgUrl = cocktailImg;
-    let imgPage = $("<img>");
-    imgPage.addClass("text-center img-fluid");
-    imgPage.attr("src", imgUrl);
-    imgPage.attr("id", "drinkPic");
-    $("#cocktails").prepend(imgPage);
   }
 };
 
@@ -189,15 +120,6 @@ let refreshFlavors = function () {
 
 $(document).ready(function () {
   console.log("document ready called");
-
-  $("#cocktails").load("/table/:id #cocktails li");
-    $("#cocktails").empty();
-    API.randomCocktail();
-
-  /*$(".search").click(function () {
-    $("ol").empty();
-    getCocktailName();
-  })*/
 
   refreshFlavors();
 
